709780c499838fe94bbb308ca30e2dd8ea675ea1
颜如冰 <yanrubing@evergrande.com>
2019-01-05 17:55:26 +0800

    utils revisee

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-import store from '../store';
+// import store from '../store';
 // 进入全屏
 export function requestFullScreen() {
   const elem = document.documentElement;
@@ -49,17 +49,13 @@ export const getValueByPath = (data, paths) => {
 
 // id to name
 export function idToName(arr, menuArr) {
-  console.log(store);
-  console.log(arr);
   let str = '';
   arr.forEach((element, index) => {
     const ind = menuArr.findIndex(item => item._id === element);
     if (ind !== -1) {
-      console.log(index, menuArr[ind].name);
       str += index ? `,${menuArr[ind].name}` : `${menuArr[ind].name}`;
     }
   });
-  console.log(str);
   if (str.startsWith(',')) {
     str = str.substring(1);
   }
@@ -103,3 +99,106 @@ export function handleObj(obj) {
   });
   return obj;
 }
+
+// 节流 情景：窗口改变，用户疯狂点击，页面滚动  原理：无论用户点击多少次，在一定时间内只算一次
+
+// export function throttle(handle, wait) {
+//   let lasttime = 0;
+//   return function(e) {
+//     console.log(e);
+//     const nowtime = new Date().getTime();
+//     if (nowtime - lasttime > wait) {
+//       handle.apply(this, arguments);
+//       lasttime = nowtime;
+//     }
+//   };
+// }
+
+// 防抖 情景：密码等校验，实时search, 拖拽   原理： 当函数频繁触发的时候， 延迟执行 setTimeout
+
+// 简单版
+// export function debounce(handle, delay) {
+//   let timer = null;
+//   return function() {
+//     const _this = this;
+
+//     const _arg = arguments;
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       handle.apply(_this, _arg);
+//     }, delay);
+//   };
+// } // 其中 handle 为需要进行防抖操作的函数，delay 为延迟时间
+
+// 增加前缘触发功能  防抖
+export const debounce = (fn, wait, immediate = false) => {
+  let timer;
+
+  let startTimeStamp = 0;
+  let context;
+  let args;
+
+  const run = timerInterval => {
+    timer = setTimeout(() => {
+      const now = new Date().getTime();
+      const interval = now - startTimeStamp;
+      if (interval < timerInterval) {
+        // the timer start time has been reset，so the interval is less than timerInterval
+        console.log('debounce reset', timerInterval - interval);
+        startTimeStamp = now;
+        run(timerInterval - interval); // reset timer for left time
+      } else {
+        if (!immediate) {
+          fn.apply(context, args);
+        }
+        clearTimeout(timer);
+        timer = null;
+      }
+    }, timerInterval);
+  };
+
+  return function() {
+    context = this;
+    args = arguments;
+    const now = new Date().getTime();
+    startTimeStamp = now; // set timer start time
+
+    if (!timer) {
+      console.log('debounce set', wait);
+      if (immediate) {
+        fn.apply(context, args);
+      }
+      run(wait); // last timer alreay executed, set a new timer
+    }
+  };
+};
+
+//
+
+// / 增加前缘  节流
+export const throttle = function(fn, interval) {
+  const __self = fn;
+  // 保存需要被延迟执行的函数引用 timer, // 定时器
+
+  let firstTime = true; // 是否是第一次调用
+  return function() {
+    const args = arguments;
+
+    const __me = this;
+    if (firstTime) {
+      // 如果是第一次调用，不需延迟执行
+      __self.apply(__me, args);
+      return (firstTime = false);
+    }
+    if (timer) {
+      // 如果定时器还在，说明前一次延迟执行还没有完成
+      return false;
+    }
+    timer = setTimeout(() => {
+      // 延迟一段时间执行
+      clearTimeout(timer);
+      timer = null;
+      __self.apply(__me, args);
+    }, interval || 500);
+  };
+};
