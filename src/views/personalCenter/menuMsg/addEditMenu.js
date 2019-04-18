128ef109787cc54a0996f773b7e349832cce2b78
sinceway <sinceway@sincewaydeMacBook-Pro.local>
2019-04-18 16:47:21 +0800

    httpS

--- src/views/personalCenter/menuMsg/addEditMenu.js
+++ src/views/personalCenter/menuMsg/addEditMenu.js
@@ -28,7 +28,7 @@ class AddEditMenu extends React.Component {
   }
 
   validateUrl = (rule, value, callback) => {
-    // const reg = /^https?:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"\"])*$/i; // 密码至少为8位的字母,数字,字符任意两种的组合
+    // const reg = /^http?:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"\"])*$/i; // 密码至少为8位的字母,数字,字符任意两种的组合
     if (value && !regular.url.test(value)) {
       callback('地址不符合规范');
     } else {
